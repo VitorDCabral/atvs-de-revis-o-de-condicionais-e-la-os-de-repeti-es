3bafeb8e187867227a5cb52b38edb8f394c657b0
VitorDCabral <168486899+VitorDCabral@users.noreply.github.com>
2024-06-03 17:25:03 -0300

    Delete aula 05 directory

--- aula 05/script.js
+++
@@ -1,9 +0,0 @@
-let numero = parseInt(prompt('Digite um numero'))
-
-if (numero < 0){
-  console.log('Este numero é negativo')
-}else if (numero === 0){
-  console.log('Este numero é zero')
-}else {
-  console.log('Este numero é positivo')
-}
