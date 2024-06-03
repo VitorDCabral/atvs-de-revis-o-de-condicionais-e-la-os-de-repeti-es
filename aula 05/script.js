32deace64fb4ec94fc2e0b1dcd39acf666f06b21
VitorDCabral <168486899+VitorDCabral@users.noreply.github.com>
2024-06-03 17:24:22 -0300

    atvs feita em aula

---
+++ aula 05/script.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt('Digite um numero'))
+
+if (numero < 0){
+  console.log('Este numero é negativo')
+}else if (numero === 0){
+  console.log('Este numero é zero')
+}else {
+  console.log('Este numero é positivo')
+}
